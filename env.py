8bc0c7a8b4c5ecde0b42dad724cacd545ac55138
MatiCava <matiascavallin96@gmail.com>
2024-12-10 01:39:41 -0300

    Commit de main y env template

---
+++ env.py
@@ -0,0 +1,6 @@
+APP_PASSWORD='password que obtuvimos recien'
+SENDER='nuestro email'
+SUBJECT_FROM='nuestro nombre o lo que queramos realmente'
+RECIPIENTS_1='a quien molestar 1'
+RECIPIENTS_2='a quien molestar 2'
+RECIPIENTS_3='a quien molestar 3'
